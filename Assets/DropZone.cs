72ee1f1eb8c2bc4c80e062f57995390ee7578ea3
Joseph Chao <jophs.chao@gmail.com>
2018-09-20 00:32:01 -0700

    Commiting code I've done on Unity so far.
    This should have a card prefab, the tabletop area, and hand area. Cards should be able to be dragged to and from the hand and tabletop.

---
+++ Assets/DropZone.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler {
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("OnPointerEnter");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("OnPointerExit");
+    }
+
+
+    public GameObject item
+    {
+        get
+        {
+            if (transform.childCount > 0)
+            {
+                return transform.GetChild(0).gameObject;
+            }
+            return null;
+        }
+    }
+
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log(eventData.pointerDrag.name + " was dropped on " + gameObject.name);
+
+        Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+        if (d != null)
+        {
+            d.parentToReturnTo = this.transform;
+        }
+    }
+
+
+}
